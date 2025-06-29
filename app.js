a68cfa7e49517c774df2ec24b5b8971922131171
RealAlive <amankrrmishra05@gmail.com>
2025-06-30 02:52:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 document.getElementById('download-link').addEventListener('click', function(event) {
     event.preventDefault();
-    window.location.href = 'https://drive.google.com/uc?export=download&id=1JF9ODFt-2Yy7QPPK7SBiWrXca4mDUU8U';
+    window.location.href = 'https://drive.usercontent.google.com/download?id=1JF9ODFt-2Yy7QPPK7SBiWrXca4mDUU8U&export=download&authuser=0&confirm=t&uuid=10f95294-59eb-4bff-93d0-27e199485a52&at=AN8xHoorJ2MZfTKmKlaEFh_XXalm%3A1751232128892';
 });
