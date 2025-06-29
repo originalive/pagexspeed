76ff277589029e0dab3e32e7e7ca163ff8fd3dcd
RealAlive <amankrrmishra05@gmail.com>
2025-06-30 02:51:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 document.getElementById('download-link').addEventListener('click', function(event) {
     event.preventDefault();
-    window.location.href = '/files/train-booking-app.zip';
+    window.location.href = 'https://drive.google.com/uc?export=download&id=1JF9ODFt-2Yy7QPPK7SBiWrXca4mDUU8U';
 });
