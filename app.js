a02816a745d17923562c8530343f7fd76d054bf6
RealAlive <amankrrmishra05@gmail.com>
2025-06-30 15:29:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-https://www.mediafire.com/upgrade/document.getElementById('download-link').addEventListener('click', function(event) {
+document.getElementById('download-link').addEventListener('click', function(event) {
     event.preventDefault();
     window.location.href = 'https://www.mediafire.com/file/1ml0k87aj17vbkb/OriginOP+v1.0.2.rar';
 });
