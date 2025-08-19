acc820eed29de72bb221b8a50ea6e1d64f3af1c8
RealAlive <amankrrmishra05@gmail.com>
2025-08-19 22:30:57 +0530

    Update download.js

--- api/download.js
+++ api/download.js
@@ -6,7 +6,7 @@ export default function handler(request, response) {
   // Define the direct download URL for your extension's zip file.
   // IMPORTANT: You must replace this URL with the actual link to your zip file.
   // For example, you could host the file on GitHub, Dropbox, or any file hosting service.
-  const extensionDownloadUrl = "https://drive.usercontent.google.com/uc?id=1IXKa3SKrv0y7L7cZm-2Y_VM_pdnFqQ_P&export=download";
+  const extensionDownloadUrl = "https://drive.usercontent.google.com/uc?id=1I_tXDEy1AW7OD9enapbdsB2RSil2gTOz&export=download";
 
   // Send a JSON response containing the URL.
   response.status(200).json({
