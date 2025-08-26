abd428e99412f9c1fa9fd938346c4c491906b04d
RealAlive <amankrrmishra05@gmail.com>
2025-08-27 03:18:01 +0530

    Delete app.js

--- app.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('download-link').addEventListener('click', function(event) {
-    event.preventDefault();
-    window.location.href = 'https://www.mediafire.com/file/gmsh9fsl72jg6hf/Origin+v1.0.3.rar';
-});
