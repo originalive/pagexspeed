50024e434c0dcb02015f8ac1aae1a34f7e32216b
RealAlive <amankrrmishra05@gmail.com>
2025-11-28 10:14:28 +0530

    Update download URL for extension

--- api/download.js
+++ api/download.js
@@ -1,5 +1,5 @@
 export default function handler(request, response) {
-const extensionDownloadUrl = "https://drive.usercontent.google.com/uc?id=1e9UqnzwqJirA8MSeMt7IYYTkCbiDicLS&export=download";
+const extensionDownloadUrl = "https://drive.usercontent.google.com/uc?id=1jv0jAPtEAIXrPuWZpNyXBtF3ZeUI35Rl&export=download";
   response.status(200).json({
     url: extensionDownloadUrl,
   });
